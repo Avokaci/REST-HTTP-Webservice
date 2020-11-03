8140a6df7279a31878f525ae99a3e1d20d25c2bf
Avokaci <if19b123@technikum-wien.at>
2020-11-03 11:43:54 +0100

    initial commit

---
+++ RESTHTTPWebservice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RESTHTTPWebservice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
